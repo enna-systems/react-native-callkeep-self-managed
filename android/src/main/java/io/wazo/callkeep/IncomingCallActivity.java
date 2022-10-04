d21146ffbde7bab93c08f6b140ee24a62fb0a3c8
Timo Köhler <t.koehler@enna.care>
2022-10-04 10:46:58 +0200

    dismiss keyguard on accept, dismiss system UI

--- android/src/main/java/io/wazo/callkeep/IncomingCallActivity.java
+++ android/src/main/java/io/wazo/callkeep/IncomingCallActivity.java
@@ -1,5 +1,7 @@
 package io.wazo.callkeep;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,77 +29,78 @@ public class IncomingCallActivity extends AppCompatActivity {
 
     public String uuid, callerName = "";
     private IncomingBroadcastReceiver incomingBroadcastReceiver;
+    private Activity mActivity = null;
 
     private static final String TAG = "RNCallKeep";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mActivity = this;
         HashMap<String, String> attributeMap = (HashMap<String, String>)getIntent().getSerializableExtra("attributeMap");
 
         uuid = attributeMap.get(EXTRA_CALL_UUID);
         callerName = attributeMap.get(EXTRA_CALLER_NAME);
-
 
         Log.i(TAG,"[IncomingCallActivity] uuid " + uuid + " callerName " + callerName);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(true);
             setTurnScreenOn(true);
-        } else {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                    | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        setContentView(R.layout.activity_incoming_call);
+        findViewById(R.id.btn_reject).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRejectClicked();
+            }
+        });
+        findViewById(R.id.btn_accept).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeyguardManager keyguardManager = (KeyguardManager)  getSystemService(Context.KEYGUARD_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+                    if(keyguardManager != null)
+                        keyguardManager.requestDismissKeyguard(mActivity, null);
+                }
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+                onAcceptClicked();
+            }
+        });
+
+        ImageView imageView = (ImageView) findViewById(R.id.avatar);
+        TextView textView = (TextView) findViewById(R.id.txt_caller_name);
+
+        Bitmap bmp = null;
+        String filename = getIntent().getStringExtra("profilePicture");
+        try {
+            FileInputStream is = this.openFileInput(filename);
+            bmp = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        boolean rejectCall = getIntent().getBooleanExtra("rejectCall", false);
-        boolean acceptCall = getIntent().getBooleanExtra("acceptCall", false);
-        if(rejectCall) {
-            onRejectClicked();
-        } else if (acceptCall) {
-            onAcceptClicked();
+        if(bmp != null) {
+            imageView.setImageBitmap(bmp);
         } else {
-            Log.i(TAG, "[IncomingCallActivity] create");
-            setContentView(R.layout.activity_incoming_call);
-            findViewById(R.id.btn_reject).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onRejectClicked();
-                }
-            });
-            findViewById(R.id.btn_accept).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onAcceptClicked();
-                }
-            });
-
-            ImageView imageView = (ImageView) findViewById(R.id.avatar);
-            TextView textView = (TextView) findViewById(R.id.txt_caller_name);
-            //byte[] byteArray = getIntent().getByteArrayExtra("profilePicture");
-
-            Bitmap bmp = null;
-            String filename = getIntent().getStringExtra("profilePicture");
-            try {
-                FileInputStream is = this.openFileInput(filename);
-                bmp = BitmapFactory.decodeStream(is);
-                is.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            if(bmp != null) {
-                // Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-                //imageView.setImageBitmap(Bitmap.createScaledBitmap(bmp, imageView.getWidth(), imageView.getHeight(), false));
-                imageView.setImageBitmap(bmp);
-                Log.i(TAG, "[IncomingCallActivity] Profile Picture");
-            } else {
-                imageView.setImageResource(R.drawable.ic_launcher_round);
-                Log.i(TAG, "[IncomingCallActivity] No Profile Picture - using enna logo");
-            }
-            textView.setText(callerName);
+            imageView.setImageResource(R.drawable.ic_launcher_round);
         }
+        textView.setText(callerName);
+
         incomingBroadcastReceiver = new IncomingBroadcastReceiver();
         registerReceiver(incomingBroadcastReceiver, new IntentFilter("finish_activity"));
         stopService(new Intent(this, NotificationService.class));
