fc8db469c47a5e9a6cb7b09f6460ce6e33d73729
Timo Köhler <t.koehler@enna.care>
2025-02-05 10:38:30 +0100

    add missing import android.media.RingtoneManager;

--- android/src/main/java/io/wazo/callkeep/VoiceConnection.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnection.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
